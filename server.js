dbbd20f28d539f3a2af9f46fc88c7cafcbddcb33
RedSoul0129 <135466675+RedSoul0129@users.noreply.github.com>
2026-02-19 13:33:23 -0500

    server.js

--- server.js
+++ server.js
@@ -157,6 +157,10 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+
   console.log("Running at http://localhost:3000");
 });
+
